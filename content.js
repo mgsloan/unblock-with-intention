070f6fc6d3ddd6baa4a4ac92e319d2f669e69a76
Michael Sloan <mgsloan@gmail.com>
2020-02-13 20:48:56 -0700

    Add "Done" button for when intention is complete

--- content.js
+++ content.js
@@ -1,3 +1,9 @@
+// TODO: really should allow renewal of time instead of hard
+// redirect, this could suck if in the middle of something.
+function redirectToBlockPage() {
+  window.location.replace('https://mgsloan.com/start-page.html?blocked=' + encodeURI(window.location));
+}
+
 const host = window.location.hostname;
 if (host === 'mgsloan.com') {
   var urlParams = new URLSearchParams(window.location.search);
@@ -37,6 +43,15 @@ if (host === 'mgsloan.com') {
 } else {
   // Omit banner / redirect on iframes.
   if (window.location === window.parent.location) {
+    chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+      switch (request.type) {
+        case 'UNPAUSE_BLOCKING': {
+          if (removeSubdomain(window.location.hostname) === request.baseDomain) {
+            redirectToBlockPage();
+          }
+        }
+      }
+    });
     chrome.runtime.sendMessage({ type: 'GET_PAUSE_INFO', host }, response => {
         const expiry = new Date(response.expiry);
         const intention = response.intention;
@@ -44,13 +59,14 @@ if (host === 'mgsloan.com') {
         const shadowDiv = document.createElement('div');
         const intentionContainerDiv = document.createElement('div');
         const intentionDiv = document.createElement('div');
+        const doneButtonDiv = document.createElement('div');
         const timerSpan = document.createElement('span');
 
         intentionContainerDiv.style['z-index'] = 2147483647
         intentionContainerDiv.style.position = 'fixed';
-        intentionContainerDiv.style.top = 0;
+        intentionContainerDiv.style.top = '0';
         intentionContainerDiv.style.width = '100%';
-        intentionContainerDiv.style['pointer-events'] = 'none';
+        intentionContainerDiv.style.pointerEvents = 'none';
 
         intentionDiv.style.display = 'table'; // for some reason this makes the centering work.
         intentionDiv.style.margin = 'auto';
@@ -61,11 +77,45 @@ if (host === 'mgsloan.com') {
         intentionDiv.style.fontSize = '30px';
         intentionDiv.style.fontFamily = 'sans';
 
+        doneButtonDiv.style.display = 'inline-block';
+        doneButtonDiv.style.position = 'relative';
+        doneButtonDiv.style.top = '5px';
+        doneButtonDiv.style.left = '5px';
+        doneButtonDiv.style.border = '1px solid white';
+        doneButtonDiv.style.margin = '0 1em';
+        doneButtonDiv.style.borderRadius = '0.5em';
+        doneButtonDiv.style.padding = '0 0.5em';
+        doneButtonDiv.style.boxShadow = '-5px -5px #f99';
+        doneButtonDiv.style.userSelect = 'none';
+        doneButtonDiv.style.pointerEvents = 'all';
+        doneButtonDiv.style.cursor = 'pointer';
+        doneButtonDiv.textContent = 'Done';
+
+        doneButtonDiv.onmousedown = ev => {
+            if (ev.button !== 0) return;
+            doneButtonDiv.style.boxShadow = '-1px -1px #f99';
+            doneButtonDiv.style.top = '1px';
+            doneButtonDiv.style.left = '1px';
+        };
+
+        doneButtonDiv.onmouseup = ev => {
+            if (ev.button !== 0) return;
+            doneButtonDiv.style.boxShadow = '-5px -5px #f99';
+            doneButtonDiv.style.top = '5px';
+            doneButtonDiv.style.left = '5px';
+        };
+
+        doneButtonDiv.onclick = ev => {
+            if (ev.button !== 0) return;
+            chrome.runtime.sendMessage({ type: 'UNPAUSE_BLOCKING', hostname: window.location.hostname });
+        };
+
         timerSpan.style.fontFamily = 'monospace';
-        timerSpan.style.marginRight = '1em';
+        timerSpan.style.margin = '0 1em';
 
         intentionDiv.appendChild(timerSpan);
         intentionDiv.appendChild(document.createTextNode("Intention: " + intention));
+        intentionDiv.appendChild(doneButtonDiv);
         intentionContainerDiv.appendChild(intentionDiv);
 
         shadowRoot = shadowDiv.attachShadow({mode: 'closed'});
@@ -78,9 +128,7 @@ if (host === 'mgsloan.com') {
         const totalExpirySeconds = Math.floor((expiry.getTime() - now.getTime()) / 1000);
         if (totalExpirySeconds < 0) {
             clearInterval(timer);
-            // TODO: really should allow renewal of time instead of hard
-            // redirect, this could suck if in the middle of something.
-            window.location.replace('https://mgsloan.com/start-page.html?blocked=' + encodeURI(window.location));
+            redirectToBlockPage();
             return;
         }
         const expiryMinutes = Math.floor(totalExpirySeconds / 60);

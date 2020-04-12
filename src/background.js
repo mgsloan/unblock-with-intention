7da7bdc10aa7918c52b937f2e78852a48def8e2e
Michael Sloan <mgsloan@gmail.com>
2020-04-11 21:46:59 -0600

    Implement functioning use of local storage

--- src/background.js
+++ src/background.js
@@ -1,88 +1,111 @@
+var state = { baseDomainMap: {} };
 const storageArea = chrome.storage.sync;
 
-const baseDomainMap = {};
+storageArea.get('state', items => {
+  try {
+    const storedState = JSON.parse(items['state'], stateReviver);
+    if (storedState) {
+      console.log('Restoring state from storage', storedState);
+      state = storedState;
+    }
+  } catch (e) {
+    console.error('Error parsing stored state', e);
+  }
+  addBeforeRequestListener();
+  // TODO: Does this means there's a race where messages might get
+  // dropped?
+  addMessageListener();
+  addCommandListener();
+});
 
-function baseDomainToKey(baseDomain) {
-  return 'domain:' + baseDomain;
+function stateReviver(key, value) {
+  if (key === 'expiry' && typeof value === 'string') {
+    return new Date(value);
+  }
+  return value;
 }
 
-function baseDomainFromKey(key) {
-  return stripPrefix('domain:', key);
+function getBaseDomainInfo(baseDomain) {
+  return state.baseDomainMap[baseDomain];
 }
 
 function setBaseDomainInfo(baseDomain, info) {
-  const key = baseDomainToKey(baseDomain);
   if (info) {
-    const items = {};
-    baseDomainMap[baseDomain] = info;
-    storageArea.set(items);
+    state.baseDomainMap[baseDomain] = info;
   } else {
-    delete baseDomainMap[baseDomain];
-    storageArea.remove(key);
+    delete state.baseDomainMap[baseDomain];
   }
+  const stateString = JSON.stringify(state);
+  console.log('Setting state to', stateString);
+  storageArea.set({ state: stateString });
 }
 
-chrome.storage.onChanged.addListener((changes, areaName) => {
-  if (areaName !== 'sync') {
-    throw 'Unexpected areaName:' + areaName;
-  }
-  for (key in changes) {
-    const change = changes[key];
-    const baseDomain = baseDomainForKey(key);
-    if (baseDomain) {
-      if (change.newValue) {
-        baseDomainMap[baseDomain] = change.newValue;
-      } else {
-        delete baseDomainMap[baseDomain];
-      }
+function addStorageChangeListener() {
+  chrome.storage.onChanged.addListener((changes, areaName) => {
+    if (areaName !== 'sync') {
+      log.error('Unexpected areaName:', areaName);
     }
-  }
-});
-
-chrome.webRequest.onBeforeRequest.addListener(
-  req => {
-    if (req.method === 'GET') {
-      const baseDomain = removeSubdomain(new URL(req.url).hostname);
-      const info = baseDomainMap[baseDomain];
-      console.log('block info = ', info);
-      if (info) {
-        if (new Date() < info.expiry) {
-          console.log('blocking paused for', baseDomain, 'so allowing request.');
-          return {};
+    for (key in changes) {
+      const change = changes[key];
+      if (key === 'state' && change.newValue) {
+        if (change.newValue) {
+          state = change.newValue;
+        } else {
+          log.error('newValue for state change not set?!');
         }
-        console.log('deleting expired domain info');
-        setBaseDomainInfo(baseDomain, null);
+      } else {
+        log.warn('Unrecognized storage change for key:', key);
       }
-      console.log('page blocked - ' + req.method + ' ' + req.url);
-      passwordParam = "";
-      if (startPagePassword) {
-        passwordParam = "&personal=t&pass=" + encodeURI(startPagePassword);
-      }
-      return {
-        redirectUrl: 'https://mgsloan.com/start-page.html?blocked=' + encodeURI(req.url) + passwordParam
-      };
-    } else {
-      console.log('non GET request to blocked page allowed - ' + req.method + ' ' + req.url);
-      return {};
     }
-  },
-  {
-    urls: [
-      '*://*.reddit.com/*',
-      '*://facebook.com/*',
-      '*://*.facebook.com/*',
-      '*://netflix.com/*',
-    ]
-  },
-  ['blocking']
-);
+  });
+}
 
-chrome.runtime.onMessage.addListener(
-  (request, sender, sendResponse) => {
+function addBeforeRequestListener() {
+  chrome.webRequest.onBeforeRequest.addListener(
+    req => {
+      if (req.method === 'GET') {
+        const baseDomain = removeSubdomain(new URL(req.url).hostname);
+        const info = getBaseDomainInfo(baseDomain);
+        // console.log('block info = ', info);
+        if (info) {
+          if (new Date() < info.expiry) {
+            // console.log('blocking paused for', baseDomain, 'so allowing request.');
+            return {};
+          }
+          console.log('deleting expired domain info');
+          setBaseDomainInfo(baseDomain, null);
+        }
+        console.log('page blocked - ' + req.method + ' ' + req.url);
+        passwordParam = "";
+        if (startPagePassword) {
+          passwordParam = "&personal=t&pass=" + encodeURI(startPagePassword);
+        }
+        return {
+          redirectUrl: 'https://mgsloan.com/start-page.html?blocked=' + encodeURI(req.url) + passwordParam
+        };
+      } else {
+        console.log('non GET request to blocked page allowed - ' + req.method + ' ' + req.url);
+        return {};
+      }
+    },
+    {
+      urls: [
+        '*://*.reddit.com/*',
+        '*://facebook.com/*',
+        '*://*.facebook.com/*',
+        '*://netflix.com/*',
+      ]
+    },
+    ['blocking']
+  );
+}
+
+function addMessageListener() {
+  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     console.log('Received', request);
     switch (request.type) {
       case 'GET_PAUSE_INFO': {
-        sendResponse(baseDomainMap[removeSubdomain(request.host)]);
+        sendResponse(getBaseDomainInfo(removeSubdomain(request.host)));
         break;
       }
       case 'PAUSE_BLOCKING': {
@@ -103,20 +126,23 @@ chrome.runtime.onMessage.addListener(
       }
     }
   });
+}
 
-chrome.commands.onCommand.addListener(function(command) {
-  console.log('Received command:', command);
-  chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-    if (tabs.length === 0) {
-      console.error('On command execution there were no active tabs');
-      return;
-    } if (tabs.length > 1) {
-      console.error('On command execution there was more than one active tab');
-    }
-    const tab = tabs[0];
-    unpauseBlocking(new URL(tab.url).hostname);
+function addCommandListener() {
+  chrome.commands.onCommand.addListener(function(command) {
+    console.log('Received command:', command);
+    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+      if (tabs.length === 0) {
+        console.error('On command execution there were no active tabs');
+        return;
+      } if (tabs.length > 1) {
+        console.error('On command execution there was more than one active tab');
+      }
+      const tab = tabs[0];
+      unpauseBlocking(new URL(tab.url).hostname);
+    });
   });
-});
+}
 
 function unpauseBlocking(hostname) {
   const baseDomain = removeSubdomain(hostname);

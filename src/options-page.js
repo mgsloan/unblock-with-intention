684ef783bf8c0612b8038a8d9f0f99dd012562e6
Michael Sloan <mgsloan@gmail.com>
2021-02-12 22:40:36 -0700

    Fix options-page.js indentation

--- src/options-page.js
+++ src/options-page.js
@@ -1,68 +1,68 @@
 {
 
-const urlInput = document.getElementById('url');
-const passwordInput = document.getElementById('password');
-const refreshMinutesInput = document.getElementById('refresh-minutes');
-const typingChallengesInput = document.getElementById('typing-challenges');
-const allowExtendInput = document.getElementById('allow-extend');
-const permissionsInput = document.getElementById('permissions');
-const permissionsButton = document.getElementById('permissions-button');
+  const urlInput = document.getElementById('url');
+  const passwordInput = document.getElementById('password');
+  const refreshMinutesInput = document.getElementById('refresh-minutes');
+  const typingChallengesInput = document.getElementById('typing-challenges');
+  const allowExtendInput = document.getElementById('allow-extend');
+  const permissionsInput = document.getElementById('permissions');
+  const permissionsButton = document.getElementById('permissions-button');
 
-function initialize() {
-  urlInput.addEventListener('input', handleChange);
-  passwordInput.addEventListener('input', handleChange);
-  refreshMinutesInput.addEventListener('input', handleChange);
-  typingChallengesInput.addEventListener('input', handleChange);
-  allowExtendInput.addEventListener('change', handleChange);
+  function initialize() {
+    urlInput.addEventListener('input', handleChange);
+    passwordInput.addEventListener('input', handleChange);
+    refreshMinutesInput.addEventListener('input', handleChange);
+    typingChallengesInput.addEventListener('input', handleChange);
+    allowExtendInput.addEventListener('change', handleChange);
 
-  chrome.runtime.sendMessage({ type: 'GET_OPTIONS' }, (options) => {
-    if (options.url) {
-      urlInput.value = options.url;
-    }
-    if (options.password) {
-      passwordInput.value = options.password;
-    }
-    if (options.refreshMinutes) {
-      refreshMinutesInput.value = options.refreshMinutes;
-    }
-    if (options.typingChallenges) {
-      typingChallengesInput.value = options.typingChallenges;
-    }
-    if (options.allowExtend) {
-      allowExtendInput.checked = true;
-    }
-    if (options.blockPatterns) {
-      currentBlockPatterns = options.blockPatterns;
-      permissionsInput.value = currentBlockPatterns.join('\n');
-    }
-  });
-
-  permissionsButton.addEventListener('click', () => {
-    const blockPatterns =
-          permissionsInput.value.split('\n')
-          .filter((ln) => ln.trim().length != 0);
-    const permissions = { origins: blockPatterns };
-    chrome.permissions.request(permissions, (granted) => {
-      if (granted) {
-        currentBlockPatterns = blockPatterns;
-        handleChange();
+    chrome.runtime.sendMessage({ type: 'GET_OPTIONS' }, (options) => {
+      if (options.url) {
+        urlInput.value = options.url;
+      }
+      if (options.password) {
+        passwordInput.value = options.password;
+      }
+      if (options.refreshMinutes) {
+        refreshMinutesInput.value = options.refreshMinutes;
+      }
+      if (options.typingChallenges) {
+        typingChallengesInput.value = options.typingChallenges;
+      }
+      if (options.allowExtend) {
+        allowExtendInput.checked = true;
+      }
+      if (options.blockPatterns) {
+        currentBlockPatterns = options.blockPatterns;
+        permissionsInput.value = currentBlockPatterns.join('\n');
       }
     });
-  });
-}
 
-function handleChange() {
-  const options = {
-    url: urlInput.value,
-    password: passwordInput.value,
-    refreshMinutes: refreshMinutesInput.value,
-    typingChallenges: typingChallengesInput.value,
-    allowExtend: allowExtendInput.checked,
-    blockPatterns: currentBlockPatterns,
-  };
-  chrome.runtime.sendMessage({ type: 'SET_OPTIONS', options });
-}
+    permissionsButton.addEventListener('click', () => {
+      const blockPatterns =
+            permissionsInput.value.split('\n')
+            .filter((ln) => ln.trim().length != 0);
+      const permissions = { origins: blockPatterns };
+      chrome.permissions.request(permissions, (granted) => {
+        if (granted) {
+          currentBlockPatterns = blockPatterns;
+          handleChange();
+        }
+      });
+    });
+  }
 
-initialize();
+  function handleChange() {
+    const options = {
+      url: urlInput.value,
+      password: passwordInput.value,
+      refreshMinutes: refreshMinutesInput.value,
+      typingChallenges: typingChallengesInput.value,
+      allowExtend: allowExtendInput.checked,
+      blockPatterns: currentBlockPatterns,
+    };
+    chrome.runtime.sendMessage({ type: 'SET_OPTIONS', options });
+  }
+
+  initialize();
 
 }
